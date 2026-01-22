656a5473d5eaed194f21daf3ba995c28c1669221
GuyBarry <107852964+GuyBarry@users.noreply.github.com>
2026-01-22 16:29:50 +0200

    add - student details activity + functionality (#3)

--- app/src/main/java/com/example/students/adapter/StudentsRecyclerAdapter.kt
+++ app/src/main/java/com/example/students/adapter/StudentsRecyclerAdapter.kt
@@ -2,10 +2,12 @@ package com.example.students.adapter
 
 import Student
 import StudentsViewHolder
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.students.R
+import com.example.students.StudentDetailsActivity
 
 class StudentsRecyclerAdapter(private val students: MutableList<Student>) : RecyclerView.Adapter<StudentsViewHolder>() {
     override fun getItemCount(): Int = students.size
@@ -19,5 +21,15 @@ class StudentsRecyclerAdapter(private val students: MutableList<Student>) : Recy
 
     override fun onBindViewHolder(holder: StudentsViewHolder, position: Int) {
         holder.bind(students[position], position)
+
+        holder.itemView.apply {
+            setOnClickListener {
+                val context = it.context
+                val intent = Intent(context, StudentDetailsActivity::class.java).apply {
+                    putExtra("studentPosition", position)
+                }
+                context.startActivity(intent)
+            }
+        }
     }
 }
